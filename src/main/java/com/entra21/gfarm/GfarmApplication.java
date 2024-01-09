9f05760fa06a71cb6d1ffbee0be3c6afd1ef961a
Iago Gondim <iago-gondim@hotmail.com>
2024-01-09 09:07:31 -0300

    commit inicial

---
+++ src/main/java/com/entra21/gfarm/GfarmApplication.java
@@ -0,0 +1,13 @@
+package com.entra21.gfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entra21/gfarm/GfarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entra21.gfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
